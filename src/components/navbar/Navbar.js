c071542406044a050e356ae6578b2529a66e5620
Hendra Wijaya <hendra.heree@gmail.com>
2021-09-25 14:13:27 +1000

    multipage done with react-router-dom

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,41 +1,39 @@
 import React from "react";
 import "./Navbar.css";
-import { Link, BrowserRouter as Router } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <Router>
-      <div className="navbar">
-        <div className="navbar-right">
-          <div className="navright-text">Hi I'm Hendra</div>
-          <div className="navbar-dropdown">
-            <ul>
-              <li>
-                <Link to="/">Skills</Link>
-              </li>
-              <li>
-                <Link to="/">Projects</Link>
-              </li>
-              <li>
-                <Link to="/">Contact Me</Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="navbar-left">
+    <div className="navbar">
+      <div className="navbar-right">
+        <div className="navright-text">Hi I'm Hendra</div>
+        <div className="navbar-dropdown">
           <ul>
             <li>
-              <Link to="/">Skills</Link>
+              <Link to="/skills">Skills</Link>
             </li>
             <li>
-              <Link to="/">Projects</Link>
+              <Link to="/projects">Projects</Link>
             </li>
             <li>
-              <Link to="/">Contact Me</Link>
+              <Link to="/contact-me">Contact Me</Link>
             </li>
           </ul>
         </div>
       </div>
-    </Router>
+      <div className="navbar-left">
+        <ul>
+          <li>
+            <Link to="/skills">Skills</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/contact-me">Contact Me</Link>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
---
+++ src/pages/skills/Skills.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { PageLayout } from "../../components/page-layout/PageLayout";
+
+export const Skills = () => {
+  return (
+    <div>
+      <PageLayout>This is Skills Page</PageLayout>
+    </div>
+  );
+};
